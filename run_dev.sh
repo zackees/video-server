a04ef711d85b362888ee0fd06637f405759b48b0
zackees <z@zackees.com>
2022-07-21 19:33:44 -0700

    fixes run_dev.sh

--- run_dev.sh
+++ run_dev.sh
@@ -1,4 +1,5 @@
 set -e
 cd $( dirname ${BASH_SOURCE[0]})
+source ./activate.sh
 python -m webbrowser -t "http://127.0.0.1:80"
 uvicorn webtorrent_movie_server.app:app --no-use-colors --reload --port 80 --host 0.0.0.0
