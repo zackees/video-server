9692de5422d828db8f7c86c0eb6dc9632ce9c7da
zackees <z@zackees.com>
2022-07-22 00:29:43 -0700

    improves install by including webtorrent/tracker

--- install_dev.sh
+++ install_dev.sh
@@ -3,3 +3,5 @@ python make_venv.py
 source activate.sh
 python -m pip install -r requirements.txt
 python -m pip install -r requirements.testing.txt
+npm install -g https://github.com/zackees/webtorrent-hybrid
+npm install -g https://github.com/zackees/bittorrent-tracker
