f1fc0fa918e5c8a0b2035dda9d94f878ad386557
zackees <z@zackees.com>
2022-07-23 22:30:21 -0700

    fix

--- install_dev.sh
+++ install_dev.sh
@@ -1,6 +1,6 @@
 cd $( dirname ${BASH_SOURCE[0]})
 python make_venv.py
-source activate.sh
+. activate.sh
 python -m pip install -r requirements.txt
 python -m pip install -r requirements.testing.txt
 npm install -g https://github.com/zackees/webtorrent-hybrid
