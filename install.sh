e95f7746e178351704b17f2c8718684b14a3cec9
zackees <z@zackees.com>
2022-07-24 01:12:48 -0700

    nit

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-python -m pip install -e . --no-cache-dir
+python -m pip install -e .
 python -m pip install -r requirements.txt
 python -m pip install -r requirements.testing.txt
 npm install -g https://github.com/zackees/webtorrent-hybrid
