26bf70d6b405818659659f3619a7d05bea54d309
zackees <z@zackees.com>
2022-08-14 00:32:06 -0700

    fix missing directory for data servering

--- http_server.sh
+++ http_server.sh
@@ -1,1 +1,8 @@
-http-server /www -p 80 --cors=* --proxy http://localhost:8000
+# if $DATA_ROOT unset then error
+if [ -z "$DATA_ROOT" ]; then
+    # Error
+    echo "DATA_ROOT is unset"
+    exit 1
+fi
+
+http-server $DATA_ROOT/www -p 80 --cors=* --proxy http://localhost:8000
