01107b853800381ddb16749f43ea40d9581be01d
zackees <z@zackees.com>
2022-11-01 20:36:44 -0700

    nit

--- video_server/app.py
+++ video_server/app.py
@@ -99,19 +99,6 @@ app.add_middleware(
 STARTUP_DATETIME = datetime.datetime.now()
 
 
-def get_video_url(url: str) -> str:
-    """Return the video url."""
-    if SERVER_PORT == 80:
-        return url
-    # parse url into parts
-    parts = urllib.parse.urlparse(url)
-    # replace the port
-    new_parts = parts._replace(netloc=f"{parts.hostname}:{SERVER_PORT}")
-    # rebuild the url
-    url = urllib.parse.urlunparse(new_parts)
-    return url
-
-
 def get_current_thread_id() -> int:
     """Return the current thread id."""
     ident = threading.current_thread().ident
